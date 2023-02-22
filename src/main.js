87a8df51e893364087edcfdc700f690686bb3630
Juanstor <alvarezjuansbs@gmail.com>
2023-02-22 14:29:30 -0500

    add main.js (testing) and styletest to change and pove some ideas

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let refLink1 = document.getElementById('.link1');
+let refLink2 = document.getElementById('.link2');
+let refLink3 = document.getElementById('.link3');
+let refLink4 = document.getElementById('.link4');
+let refLink5 = document.getElementById('.link5');
+
+refLink1.addEventListener(click, function() {
+    window.location.href = 'https://platzi.com/blog/ganadores-filminutochallenge/';
+});
+refLink2.addEventListener(click, function() {
+    window.location.href = 'https://youtu.be/Hsq8lPZACuw';
+});
+refLink3.addEventListener(click, function() {
+    window.location.href = 'https://youtube.com/shorts/8TPmcIxS83Y?feature=share';
+});
