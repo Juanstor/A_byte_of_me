1f3dbf5ba771dbe4ccf68e902f94b032c8fec096
Juanstor <alvarezjuansbs@gmail.com>
2023-02-22 15:11:25 -0500

    modify index and main to create events and the user can redirect to youtube and others important pages

---
+++ src/assets/main.js
@@ -0,0 +1,14 @@
+let refLink1 = document.querySelector('.link1');
+let refLink2 = document.querySelector('.link2');
+let refLink3 = document.querySelector('.link3');
+
+
+refLink1.addEventListener('click', function() {
+    window.location.href = 'https://platzi.com/blog/ganadores-filminutochallenge/';
+});
+refLink2.addEventListener('click', function() {
+    window.location.href = 'https://youtu.be/Hsq8lPZACuw';
+});
+refLink3.addEventListener('click', function() {
+    window.location.href = 'https://youtube.com/shorts/8TPmcIxS83Y?feature=share';
+});
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-let refLink1 = document.getElementById('.link1');
-let refLink2 = document.getElementById('.link2');
-let refLink3 = document.getElementById('.link3');
-let refLink4 = document.getElementById('.link4');
-let refLink5 = document.getElementById('.link5');
+// let refLink1 = document.querySelector('.link1');
+// let refLink2 = document.querySelector('.link2');
+// let refLink3 = document.querySelector('.link3');
+// let refLink4 = document.querySelector('.link4');
+// let refLink5 = document.querySelector('.link5');
 
-refLink1.addEventListener(click, function() {
-    window.location.href = 'https://platzi.com/blog/ganadores-filminutochallenge/';
-});
-refLink2.addEventListener(click, function() {
-    window.location.href = 'https://youtu.be/Hsq8lPZACuw';
-});
-refLink3.addEventListener(click, function() {
-    window.location.href = 'https://youtube.com/shorts/8TPmcIxS83Y?feature=share';
-});
+// refLink1.addEventListener('click', function() {
+//     window.location.href = 'https://platzi.com/blog/ganadores-filminutochallenge/';
+// });
+// refLink2.addEventListener('click', function() {
+//     window.location.href = 'https://youtu.be/Hsq8lPZACuw';
+// });
+// refLink3.addEventListener('click', function() {
+//     window.location.href = 'https://youtube.com/shorts/8TPmcIxS83Y?feature=share';
+// });
